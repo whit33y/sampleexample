a69dd93e0fc07147e4d70bed6c466ea344d18702
whit33y <troppout@gmail.com>
2025-12-07 17:47:51 +0100

    feat: add routes and page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/auth-page').then((m) => m.AuthPage),
+  },
+  {
+    path: 'categories',
+    loadComponent: () => import('./pages/categories/categories-page').then((m) => m.CategoriesPage),
+  },
+  {
+    path: 'samples',
+    loadComponent: () => import('./pages/samples/samples-page').then((m) => m.SamplesPage),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found-page').then((m) => m.NotFoundPage),
+  },
+];
---
+++ src/app/pages/auth/auth-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth-page.html',
+  styleUrl: './auth-page.scss',
+})
+export class AuthPage {}
